604c97185e5f0b0162a244ae9d9f1a7bfea21ebd
Victini00 <kwonsy1446@snu.ac.kr>
2025-03-31 23:46:00 +0900

    3/31

--- IML_Assignment 1/pa1.py
+++ IML_Assignment 1/pa1.py
@@ -18,14 +18,14 @@ def prepare_dataset(path: Path) -> Tuple[np.ndarray, np.ndarray]:
     # NaN 값에 대한 예외 처리 가능
     data = np.genfromtxt(Path(path), delimiter=',', skip_header=1)
     
-    labels = data[:,0].astype(np.int64)
-    pixels = data[:,1:]
+    Labels = data[:,0].astype(np.int64)
+    pixel_value = data[:,1:]
 
     # 크기 조작 및 타입 변경
-    images = pixels.reshape(labels.shape[0], 16, 16)
-    images = images.astype(np.float32)/255.0
+    Images = pixel_value.reshape(Labels.shape[0], 16, 16)
+    Images = Images.astype(np.float32)/255.0
 
-    return images,labels
+    return Images, Labels
 
 ## 2. Write the kernel function
 def kernel_function(a: np.ndarray, b: np.ndarray, sigma: float = 1.0) -> float:
@@ -35,13 +35,17 @@ def kernel_function(a: np.ndarray, b: np.ndarray, sigma: float = 1.0) -> float:
     Returns:
         single scalar value.
     """
-    ...
+    euc_dis = np.sum((a-b)**2)
+
+    kernel_value = np.exp(-euc_dis / (2 *sigma**2))
+    
+    return kernel_value
 
 
 ## 3. Update the parameters
 def train(
-    dataset: Tuple[np.ndarray, np.ndarray],
-    num_training_steps: int,
+    dataset: Tuple[np.ndarray, np.ndarray], # 1의 리턴값
+    num_training_steps: int, 
     learning_rate: float,
     sigma: float = 1.0,
 ) -> np.ndarray:
@@ -55,8 +59,29 @@ def train(
     Returns:
         [np.float32; [B]], the trained parameters.
     """
-    ...
 
+    kernel = [[0 for _ in range(dataset[0].shape[0])] for _ in range(dataset[0].shape[0])]
+
+    # 커널 값 계산
+    for i in range(dataset[0].shape[0]):
+        for j in range(dataset[0].shape[0]):
+            kernel[i][j] = kernel_function(dataset[0][i], dataset[0][j], sigma)
+
+    init_beta = np.zeros((dataset[0].shape[0]))
+
+    beta = np.zeros((dataset[0].shape[0]))
+
+    for _ in range(num_training_steps):
+        for i in range(dataset[0].shape[0]):
+            kernel_sum = 0
+            for j in range(dataset[0].shape[0]):
+                kernel_sum += init_beta[j] * kernel[i][j]
+            beta[i] = init_beta[i] + learning_rate * (dataset[1][i] - kernel_sum)
+
+        init_beta = beta
+        beta = np.zeros((dataset[0].shape[0]))
+
+    return init_beta
 
 ## 4. Estimate the labels of the given images.
 def classify(model: Tuple[np.ndarray, np.ndarray], given: np.ndarray) -> int:
@@ -67,4 +92,4 @@ def classify(model: Tuple[np.ndarray, np.ndarray], given: np.ndarray) -> int:
     Returns:
         an estimated label of the image.
     """
-    ...
+    
